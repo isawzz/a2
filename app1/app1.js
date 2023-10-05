9db66334400fb453d82ee69b0cc06ce6ab2578c3
isawzz <iwgajam@gmail.com>
2023-10-04 18:22:23 -0700

    das

--- app1/app1.js
+++ app1/app1.js
@@ -4,7 +4,7 @@ app.use(express.static(__dirname + '/..')); //Serve root directory
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-app.get('/app1', (req, res) => {
+app.get('/node', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 app.listen(3000);
